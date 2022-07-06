d4e3eb58c4d287454f6099a0da945fffaebb3007
jeonjihyeong <102641699+jeonjihyeong@users.noreply.github.com>
2022-07-06 12:15:31 +0900

    data base(postgres)연동 + 데이터 베이스 호출하여  로그인 기능 구현, 회원가입 기능, sequelize

--- MFI/server/index.js
+++ MFI/server/index.js
@@ -3,32 +3,51 @@ const app = express()
 const port = 3000
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const db= require('./db')
+const service=require('./service')
+
+
+
 app.use(bodyParser.json())
 app.use(cors());
 
-const db = {
-  id : 'aaa',
-  pw : 'aaa'
-} 
-
-app.get('/id/:id',(req,res)=>{
-  console.log(req.params)
-  res.send(db)
+// app.get('/ping',async(req,res)=>{
+//   const ress = await service.getUserId();
   
+//   console.log(ress.id)
+
+// })
+
+app.post('/signup',async(req,res)=>{
+  console.log(1)
+  const result = await service.saveUser('bv','bv')
+  console.log(result)
 })
 
-app.post('/login', (req, res) => {
-  if(req.body.id === db.id){
-    if(req.body.pw === db.pw){
-      res.send('success');
-    }else {
-      res.send('failpw');
-    }
+// app.get('/id/:id',(req,res)=>{
+//   console.log(req.params)
+//   res.send(db)
+  
+// })
+
+
+
+app.post('/login', async(req, res) => {
+  const data=req.body;
+  const idData=await service.getUserId(data.id);
+  if (idData===null){
+    res.send ({data: 'not exist'})
   }else {
-    res.send('failid');
+      const pwData=idData.dataValues.pw
+      if(data.pw!==pwData){
+          res.send({data: 'wrong password'})
+      }else {
+          res.send({data: 1});
+      }
   }
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`)
+  await db.initialize()
 })
